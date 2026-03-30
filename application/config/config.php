daa671e4c399a1e2885de2c12b85865878efdbb5
Shubham Jogi <moonit@sezindia.co.in>
2026-03-30 12:52:38 +0530

    config_commit

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,11 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/aptitude_exam1/';
+// $config['base_url'] = 'http://localhost/aptitude_exam1/';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
 
 /*
 |--------------------------------------------------------------------------
